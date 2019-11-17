4f71b912510416e44da1d8f6cd64fa72171fd2d5
JACKSUYON <57020189+jacksuyon@users.noreply.github.com>
2019-11-16 19:58:11 -0600

    se agrego el lote.bat ....trabajo terminado

---
+++ lote.bat
@@ -0,0 +1,270 @@
+python suyon/condicionales_simples/condicional_simple01.py "joel" 23.3 4.5 46.8
+python suyon/condicionales_simples/condicional_simple01.py "rosa" 2.3 45 46
+python suyon/condicionales_simples/condicional_simple01.py "leonel" 27 52 6.8
+
+python suyon/condicionales_simples/condicional_simple02.py 34 10 5
+python suyon/condicionales_simples/condicional_simple02.py 4 9.8 15
+python suyon/condicionales_simples/condicional_simple02.py 5 9.7 6
+
+python suyon/condicionales_simples/condicional_simple03.py 120 4 2
+python suyon/condicionales_simples/condicional_simple03.py 340 1 3
+python suyon/condicionales_simples/condicional_simple03.py 23 2 1
+
+python suyon/condicionales_simples/condicional_simple04.py "sonia" 340 100 75
+python suyon/condicionales_simples/condicional_simple04.py "tania" 400 100 3
+python suyon/condicionales_simples/condicional_simple04.py "carlos" 400 13 5
+
+python suyon/condicionales_simples/condicional_simple05.py 10 5
+python suyon/condicionales_simples/condicional_simple05.py 34 10
+python suyon/condicionales_simples/condicional_simple05.py 4 1
+
+python suyon/condicionales_simples/condicional_simple06.py 44 50
+python suyon/condicionales_simples/condicional_simple06.py 24 1
+python suyon/condicionales_simples/condicional_simple06.py 3 5
+
+python suyon/condicionales_simples/condicional_simple07.py 35 16 64 6
+python suyon/condicionales_simples/condicional_simple07.py 30 9 1 3
+python suyon/condicionales_simples/condicional_simple07.py 7 10 2 1
+
+python suyon/condicionales_simples/condicional_simple08.py 45 5
+python suyon/condicionales_simples/condicional_simple08.py 3 6
+python suyon/condicionales_simples/condicional_simple08.py 13 5
+
+python suyon/condicionales_simples/condicional_simple09.py 4 103
+python suyon/condicionales_simples/condicional_simple09.py 45 16
+python suyon/condicionales_simples/condicional_simple09.py 94 1
+
+python suyon/condicionales_simples/condicional_simple10.py 106 20 15
+python suyon/condicionales_simples/condicional_simple10.py 100 125 25
+python suyon/condicionales_simples/condicional_simple10.py 340 10 5
+
+
+
+
+python suyon/condicionales_dobles/condicional_doble01.py "jesus" 32 45 12
+python suyon/condicionales_dobles/condicional_doble01.py "felipe" 2 5 32
+python suyon/condicionales_dobles/condicional_doble01.py "carlos" 32 2 7
+
+python suyon/condicionales_dobles/condicional_doble02.py 3 45 12
+python suyon/condicionales_dobles/condicional_doble02.py 32 4 1
+python suyon/condicionales_dobles/condicional_doble02.py 1 1 2
+
+python suyon/condicionales_dobles/condicional_doble03.py 32 2 1
+python suyon/condicionales_dobles/condicional_doble03.py 65 5 2
+python suyon/condicionales_dobles/condicional_doble03.py 340 3 2
+
+python suyon/condicionales_dobles/condicional_doble04.py "julio" 234 45 674
+python suyon/condicionales_dobles/condicional_doble04.py "esteban" 350 34 26
+python suyon/condicionales_dobles/condicional_doble04.py "jack" 23 457 54
+
+python suyon/condicionales_dobles/condicional_doble05.py 234 45
+python suyon/condicionales_dobles/condicional_doble05.py 33 3
+python suyon/condicionales_dobles/condicional_doble05.py 12 3
+
+python suyon/condicionales_dobles/condicional_doble06.py 9 1
+python suyon/condicionales_dobles/condicional_doble06.py 34 2
+python suyon/condicionales_dobles/condicional_doble06.py 3 7
+
+python suyon/condicionales_dobles/condicional_doble07.py 32 6 1
+python suyon/condicionales_dobles/condicional_doble07.py 2 5 2
+python suyon/condicionales_dobles/condicional_doble07.py 34 56 9
+
+python suyon/condicionales_dobles/condicional_doble08.py 43 2
+python suyon/condicionales_dobles/condicional_doble08.py 12 5
+python suyon/condicionales_dobles/condicional_doble08.py 9 1
+
+python suyon/condicionales_dobles/condicional_doble09.py 45 67
+python suyon/condicionales_dobles/condicional_doble09.py 23 6
+python suyon/condicionales_dobles/condicional_doble09.py 66 2
+
+python suyon/condicionales_dobles/condicional_doble10.py 43 32 45
+python suyon/condicionales_dobles/condicional_doble10.py 98 56 23
+python suyon/condicionales_dobles/condicional_doble10.py 345 72 65
+
+
+
+
+
+
+python suyon/condicionales_multiples/condicional_multiple01.py "daniel" 34 34 2
+python suyon/condicionales_multiples/condicional_multiple01.py "daniel" 34 34 2
+python suyon/condicionales_multiples/condicional_multiple01.py "daniel" 34 34 2
+
+python suyon/condicionales_multiples/condicional_multiple02.py 2 4 1
+python suyon/condicionales_multiples/condicional_multiple02.py -3 34 5
+python suyon/condicionales_multiples/condicional_multiple02.py 3 20 3
+
+python suyon/condicionales_multiples/condicional_multiple03.py 234 23 5
+python suyon/condicionales_multiples/condicional_multiple03.py 120 4 20
+python suyon/condicionales_multiples/condicional_multiple03.py 900 29 3
+
+python suyon/condicionales_multiples/condicional_multiple04.py "sebastian" 320 100 3
+python suyon/condicionales_multiples/condicional_multiple04.py "enrique" 400 9 7
+python suyon/condicionales_multiples/condicional_multiple04.py "lucia" 210 200 1
+
+python suyon/condicionales_multiples/condicional_multiple05.py 43 -5
+python suyon/condicionales_multiples/condicional_multiple05.py 32 4
+python suyon/condicionales_multiples/condicional_multiple05.py 21 3
+
+python suyon/condicionales_multiples/condicional_multiple06.py 5 3
+python suyon/condicionales_multiples/condicional_multiple06.py 34 65
+python suyon/condicionales_multiples/condicional_multiple06.py 2 1
+
+python suyon/condicionales_multiples/condicional_multiple07.py 32 3 1 7
+python suyon/condicionales_multiples/condicional_multiple07.py 21 8 5
+python suyon/condicionales_multiples/condicional_multiple07.py 11 3 2
+
+python suyon/condicionales_multiples/condicional_multiple08.py 23 45
+python suyon/condicionales_multiples/condicional_multiple08.py 9 17
+python suyon/condicionales_multiples/condicional_multiple08.py 6 4
+
+python suyon/condicionales_multiples/condicional_multiple09.py 36 76
+python suyon/condicionales_multiples/condicional_multiple09.py 51 3
+python suyon/condicionales_multiples/condicional_multiple09.py 23 75
+
+python suyon/condicionales_multiples/condicional_multiple10.py 40 300 34
+python suyon/condicionales_multiples/condicional_multiple10.py 118 108 30
+python suyon/condicionales_multiples/condicional_multiple10.py 47 109 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+python sandoval/simples/ejercicio01.py "Juan" 2 5 6 30.5 20.2 30.0 8.8
+python sandoval/simples/ejercicio01.py "Jose" 4 1 2 25.5 63.0 24.2 5.4
+python sandoval/simples/ejercicio01.py "Lucas" 8 2 7 24.2 42.2 40.2 2.8
+
+python sandoval/simples/ejercicio02.py "Ruben" 500 0.5
+python sandoval/simples/ejercicio02.py "Carlos" 850 1.5
+python sandoval/simples/ejercicio02.py "Santos" 300 0.2
+
+python sandoval/simples/ejercicio03.py "Erick" "Empleado" 1500 200
+python sandoval/simples/ejercicio03.py "Elmer" "chofer" 1300 100
+python sandoval/simples/ejercicio03.py "Joel" "contador" 1800 183
+
+python sandoval/simples/ejercicio04.py 20 10
+python sandoval/simples/ejercicio04.py 15 13
+python sandoval/simples/ejercicio04.py 30 8
+
+python sandoval/simples/ejercicio05.py 20 3.14
+python sandoval/simples/ejercicio05.py 15 3.14
+python sandoval/simples/ejercicio05.py 8 3.14
+
+python sandoval/simples/ejercicio06.py 8 12 9.8
+python sandoval/simples/ejercicio06.py 15 16 9.8
+python sandoval/simples/ejercicio06.py 25 18 9.8
+
+python sandoval/simples/ejercicio07.py 10 20 5
+python sandoval/simples/ejercicio07.py 15 25 8
+python sandoval/simples/ejercicio07.py 30 10 6
+
+python sandoval/simples/ejercicio08.py 10 8
+python sandoval/simples/ejercicio08.py 15 4
+python sandoval/simples/ejercicio08.py 20 12
+
+python sandoval/simples/ejercicio09.py 15 25 6
+python sandoval/simples/ejercicio09.py 14 16 2
+python sandoval/simples/ejercicio09.py 18 12 7
+
+python sandoval/simples/ejercicio10.py 4 5
+python sandoval/simples/ejercicio10.py 13 2
+python sandoval/simples/ejercicio10.py 8 3
+
+python sandoval/dobles/ejercicio11.py 10 8 5
+python sandoval/dobles/ejercicio11.py 20 6 6
+python sandoval/dobles/ejercicio11.py 25 8 4
+
+python sandoval/dobles/ejercicio12.py 10 25
+python sandoval/dobles/ejercicio12.py 15 13
+python sandoval/dobles/ejercicio12.py 13 25
+
+python sandoval/dobles/ejercicio13.py 14 8
+python sandoval/dobles/ejercicio13.py 18 2
+python sandoval/dobles/ejercicio13.py 25 6
+
+python sandoval/dobles/ejercicio14.py 10 9
+python sandoval/dobles/ejercicio14.py 12 8
+python sandoval/dobles/ejercicio14.py 14 7
+
+python sandoval/dobles/ejercicio15.py 10 20 15
+python sandoval/dobles/ejercicio15.py 15 14 8
+python sandoval/dobles/ejercicio15.py 18 23 7
+
+python sandoval/dobles/ejercicio16.py 25 9.8
+python sandoval/dobles/ejercicio16.py 15 9.8
+python sandoval/dobles/ejercicio16.py 14 9.8
+
+python sandoval/dobles/ejercicio17.py 25 8
+python sandoval/dobles/ejercicio17.py 14 6
+python sandoval/dobles/ejercicio17.py 18 9
+
+python sandoval/dobles/ejercicio18.py "JOSE" 2 5 4 25.2 32.5 48.1 5.1
+python sandoval/dobles/ejercicio18.py "Roberto" 4 8 6 21.5 12.4 48.5 2.3
+python sandoval/dobles/ejercicio18.py "Cristian" 7 2 9 12.5 10.0 4.5 62.5
+
+python sandoval/dobles/ejercicio19.py "Maria" 2 8 12.5 14.0 1.5
+python sandoval/dobles/ejercicio19.py "Juana" 1 4 13.5 21.1 1.2
+python sandoval/dobles/ejercicio19.py "Carlos" 4 2 16.2 14.5 2.14
+
+python sandoval/dobles/ejercicio20.py 20 14 9.8
+python sandoval/dobles/ejercicio20.py 13 26 9.8
+python sandoval/dobles/ejercicio20.py 4 12 9.8
+
+python sandoval/multiples/ejercicio21.py "JOSE" 2 5 4 25.2 32.5 48.1 5.1
+python sandoval/multiples/ejercicio21.py "LUCAS" 4 3 6 22.4 21.1 5.4 0.5
+python sandoval/multiples/ejercicio21.py "RUBEN" 5 7 4 21.4 85.0 45.0 1.2
+
+python sandoval/multiples/ejercicio22.py "YOEL" 1000 0.5
+python sandoval/multiples/ejercicio22.py "MARCOS" 1500 1.2
+python sandoval/multiples/ejercicio22.py "JHOAN" 1200 1.15
+
+python sandoval/multiples/ejercicio23.py "GRABIEL" "COCINERO" 1500 100
+python sandoval/multiples/ejercicio23.py "MARICIELO" "MESERA" 600 20
+python sandoval/multiples/ejercicio23.py "TITO" "COBRADOR " 500 30
+
+python sandoval/multiples/ejercicio24.py 20 10 9.8
+python sandoval/multiples/ejercicio24.py 50 20 9.8
+python sandoval/multiples/ejercicio24.py 25 14 9.8
+
+python sandoval/multiples/ejercicio25.py 10 20 8
+python sandoval/multiples/ejercicio25.py 54 23 9
+python sandoval/multiples/ejercicio25.py 26 14 12
+
+python sandoval/multiples/ejercicio26.py 15 25 8
+python sandoval/multiples/ejercicio26.py 12 16 4
+python sandoval/multiples/ejercicio26.py 45 12 5
+
+python sandoval/multiples/ejercicio27.py 50 4
+python sandoval/multiples/ejercicio27.py 80 15
+python sandoval/multiples/ejercicio27.py 56 12
+
+python sandoval/multiples/ejercicio28.py 10 4 5
+python sandoval/multiples/ejercicio28.py 1 4 8
+python sandoval/multiples/ejercicio28.py 12 4 8
+
+python sandoval/multiples/ejercicio29.py 10 20 2
+python sandoval/multiples/ejercicio29.py 15 25 4
+python sandoval/multiples/ejercicio29.py 12 48 7
+
+python sandoval/multiples/ejercicio30.py "MATEO" 5 4 21 12 8.8
+python sandoval/multiples/ejercicio30.py "ALEX" 4 7 5 30 1.2
+python sandoval/multiples/ejercicio30.py "ALEXANDRA" 8 5 14 18 0.6
