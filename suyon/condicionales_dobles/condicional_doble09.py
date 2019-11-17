b9ac9ac858cec289db3a50f016a30d9bd8cf5986
SUYON <jsuyonr@unprg.edu.pe>
2019-11-16 18:36:56 -0600

    se agrego el condicional doble 09

---
+++ suyon/condicionales_dobles/condicional_doble09.py
@@ -0,0 +1,29 @@
+import os
+#calcular el area total de la piramide
+
+#input
+area_lateral_piramide=int(os.sys.argv[1])
+area_base_piramide=int(os.sys.argv[2])
+
+#processing
+area_total_piramide= area_lateral_piramide + area_base_piramide
+
+#verificador
+verificador=(area_total_piramide<=72)
+
+#output
+print("#################################################")
+print("#        BOLETA DEL AREA TOTAL DE LA PIRAMIDE    #")
+print("#################################################")
+print("# datos  "                                        )
+print("#area lateral de la piramide:    ",area_lateral_piramide)
+print("#area de la base de la piramide:    ",area_base_piramide)
+print("#area total de la piramide     ",area_total_piramide)
+print("###################################################")
+
+#condicional doble
+#si el area de la piramide <72 mostrar,piramide estable
+if(verificador==True):
+    print("piramide estable")
+else:
+    print("piramide inestable")
