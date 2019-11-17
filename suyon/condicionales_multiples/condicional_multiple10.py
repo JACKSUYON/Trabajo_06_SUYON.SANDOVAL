478bcb9034c9380d20e5b5661ea4b322dc2cacd8
SUYON <jsuyonr@unprg.edu.pe>
2019-11-16 18:58:09 -0600

    se agrego el condicional multiple 10
    trabajo de suyon terminado

---
+++ suyon/condicionales_multiples/condicional_multiple10.py
@@ -0,0 +1,33 @@
+import os
+#calcular la compra de gasolina por galones de un vehiculo pesado el grifo Primax
+
+#input
+compra1=int(os.sys.argv[1])
+compra2=int(os.sys.argv[2])
+compra3=int(os.sys.argv[3])
+
+#processing
+compra_total=compra1+compra2+compra3
+
+#verificador
+verificador=compra_total>=300
+
+#output
+print("#################################")
+print("# BOLETA DE GALONES (PRIMAX)")
+print("#################################")
+print("#compra1:",compra1)
+print("#compra2:",compra2)
+print("#compra3:",compra3)
+print("#compra total:",compra_total)
+print("########################################")
+
+#condicional multiple
+#si la compra total>300 mostrar, felicitaciones ganaste un celular nokia 2.1
+if(verificador==True):
+    print("felicitaciones , ganaste un celular nokia 2.1")
+if(compra_total<=300 and compra_total>=250):
+    print("estas a punto de ganar un celular nokia 2.1")
+if(compra_total<250 and compra_total>100):
+    print("tienes un descuento de %41")
+#fin_if
