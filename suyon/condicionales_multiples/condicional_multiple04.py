73951e89ed24e92965a8d521f1b6d383264a86b5
SUYON <jsuyonr@unprg.edu.pe>
2019-11-16 18:51:13 -0600

    se agrego el condicional multiple 04

---
+++ suyon/condicionales_multiples/condicional_multiple04.py
@@ -0,0 +1,35 @@
+import os
+# clacular el numero de ventas
+
+#input
+trabajador=os.sys.argv[1]
+venta1=float(os.sys.argv[2])
+venta2=float(os.sys.argv[3])
+venta3=float(os.sys.argv[4])
+
+#processing
+total_de_ventas_al_dia=(venta1+venta2+venta3)
+
+#verificador
+verificador=total_de_ventas_al_dia>520
+
+#ouput
+print("####################################")
+print("# VENTAS DEL DIA ")
+print("#######################################")
+print("#trabajador:",trabajador)
+print("venta1:",venta1)
+print("#venta2:",venta2)
+print("#venta3:",venta3)
+print("#total de ventas al dia:",total_de_ventas_al_dia)
+print("##############################################")
+
+#condicional multiple
+#si el total de ventas>520 mostrar, ganaste 20 soles adicionales
+if(verificador==True):
+    print("ganaste 20 soles adicionales")
+if(total_de_ventas_al_dia<=520 and total_de_ventas_al_dia>500):
+    print("eres un vendedor eficiente")
+if(total_de_ventas_al_dia<=500 and total_de_ventas_al_dia>400):
+    print("eres un trabajador eficaz")
+#fin_if
