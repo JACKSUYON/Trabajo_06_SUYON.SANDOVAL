b52259bdad651b38b4d64a31568a7047070701f6
SUYON <jsuyonr@unprg.edu.pe>
2019-11-16 18:36:07 -0600

    se agrego el condicional doble 08

---
+++ suyon/condicionales_dobles/condicional_doble08.py
@@ -0,0 +1,24 @@
+import os
+#calcular el numero de electrones
+#imput
+numero_de_atomico=int(os.sys.argv[1])
+carga=int(os.sys.argv[2])
+
+#processing
+numero_de_electrones=(numero_de_atomico+carga)
+
+#output
+print("###########################################")
+print("#   NUMERO DE ELECTRONES          ")
+print("###########################################")
+print("#numero de atomico:",numero_de_atomico)
+print("#carga:",carga)
+print("#el numero de electrones:",numero_de_electrones)
+print("##############################################")
+
+#condicional doble
+#si el numero de elctrones>12 mostrarle,maximo numero de electrones
+if(numero_de_electrones>12):
+    print("maximo numero de electrones ")
+else:
+    print("numero de elctrones minimo")
