9b48448aefbcbb1c1e8eea8094b1414fd52e9fe8
SUYON <jsuyonr@unprg.edu.pe>
2019-11-16 18:47:47 -0600

    se agrego el condicional multiple 01

---
+++ suyon/condicionales_multiples/condicional_multiple01.py
@@ -0,0 +1,35 @@
+import os
+#calcular el puntaje del examen ordinario de la Universidad Nacional Pedro Ruis Gallo
+
+#input
+postulante=os.sys.argv[1]
+puntaje_letras=float(os.sys.argv[2])
+puntaje_numeros=float(os.sys.argv[3])
+puntaje_humanidades=float(os.sys.argv[4])
+
+#processing
+puntaje_total=puntaje_letras+puntaje_numeros+puntaje_humanidades
+
+#verificador
+verificador=puntaje_total>=65.5
+
+#output
+print("###########################################")
+print("# Puntaje del postulante")
+print("#########################################")
+print("#postulante:",postulante)
+print("#puntaje de letras:",puntaje_letras)
+print("#puntaje de numeros:",puntaje_numeros)
+print("#puntaje de humanidades:",puntaje_humanidades)
+print("#puntaje total:",puntaje_total)
+print("#############################################")
+
+#condicion multiple
+#si el postulante tiene un putaje>=65.5 mostrar ,Alcanzo vacante
+if(verificador==True):
+    print("Alcanzo vacante")
+if(puntaje_total==90):
+    print("computo general")
+if(puntaje_total<65.5 and puntaje_total>35):
+    print("estas en competencia")
+#fin_if
